acf1098def320bd31ea19e664d022b5f4a83c72a
Andrey Fernando Ortiz Rodriguez <andreymafesara@hotmail.com>
2018-02-13 17:15:28 -0500

    Preliminar rutas angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+// Rutas
+import { APP_ROUTING } from './app.routes';
 
+// Servicios
+
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -18,7 +25,10 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
